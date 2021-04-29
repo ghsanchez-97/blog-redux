d2a2dc60f50abbab68bf815d991182e41e5bc253
Guy Sanchez <anakin25@live.com>
2021-04-29 16:41:58 -0600

    Inicio de proyecto de redux

---
+++ src/components/App.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import './styles/App.css';
+import axios from 'axios';
+
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      users: [],
+    }
+  }
+
+  async componentDidMount() {
+    const res = await axios.get('https://jsonplaceholder.typicode.com/users');
+    this.setState({
+      users: res.data
+    })
+  }
+
+  putFiles = () => (
+    this.state.users.map((user) =>(
+      <tr key={user.id}>
+        <td>
+          {user.name}
+        </td>
+        <td>
+          {user.email}
+        </td>
+        <td>
+          {user.website}
+        </td>
+      </tr>
+    ))
+  );
+
+  render() {
+    return (
+      <div className="margen">
+        <table className="tabla">
+          <thead>
+            <tr>
+              <th>
+                Nombre
+              </th>
+              <th>
+                Correo
+              </th>
+              <th>
+                Enlace
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+           { this.putFiles() }
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default App;
