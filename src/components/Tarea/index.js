aec350c0ca74e0e2f278af6dfca1096a71b5fc8d
Guy Sanchez <anakin25@live.com>
2021-05-01 10:39:36 -0600

    Nuevas pestaña e configuración

---
+++ src/components/Tarea/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Tareas = () => (
+    <React.Fragment>
+        <div>
+            <h1>Tarea</h1>
+        </div>
+    </React.Fragment>
+)
+export default Tareas;
