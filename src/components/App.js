2525f53ccef1294316113e2533fdd869c09c9f5d
Guy Sanchez <anakin25@live.com>
2021-05-01 10:36:59 -0600

    Instalación de react-router-dom y navegación entre pestaña de la App

--- src/components/App.js
+++ src/components/App.js
@@ -1,62 +1,14 @@
 import React from 'react';
-import './styles/App.css';
-import axios from 'axios';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Menu from './Menu';
+import Users from './Users';
+import Tareas from './Tarea';
 
-class App extends React.Component{
-  constructor(){
-    super();
-    this.state = {
-      users: [],
-    }
-  }
-
-  async componentDidMount() {
-    const res = await axios.get('https://jsonplaceholder.typicode.com/users');
-    this.setState({
-      users: res.data
-    })
-  }
-
-  putFiles = () => (
-    this.state.users.map((user) =>(
-      <tr key={user.id}>
-        <td>
-          {user.name}
-        </td>
-        <td>
-          {user.email}
-        </td>
-        <td>
-          {user.website}
-        </td>
-      </tr>
-    ))
-  );
-
-  render() {
-    return (
-      <div className="margen">
-        <table className="tabla">
-          <thead>
-            <tr>
-              <th>
-                Nombre
-              </th>
-              <th>
-                Correo
-              </th>
-              <th>
-                Enlace
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-           { this.putFiles() }
-          </tbody>
-        </table>
-      </div>
-    );
-  }
-}
-
+const App = () => (
+  <BrowserRouter>
+    <Menu />
+    <Route exact path='/' component={Users} />
+    <Route exact path='/tareas' component={Tareas} />
+  </BrowserRouter>
+)
 export default App;
