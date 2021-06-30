c4f54d7e7801652b7024da90b9bd34cdc67256c7
Guy Sanchez <anakin25@live.com>
2021-06-29 19:55:57 -0600

    :construction: - Se agrego publics_key, ademas ya muestra el nombre y se coloco el loading y error, se destructuro algunas funciones

--- src/components/Publicaciones/index.js
+++ src/components/Publicaciones/index.js
@@ -2,24 +2,59 @@ import React from "react";
 import { connect } from "react-redux";
 import * as userActions from "../../actions/userActions";
 import * as publicActions from "../../actions/publicActions";
+import Spinner from "../General/Spinner";
+import Error from "../General/Error"; 
 
 const { getAll: usersGetAll } = userActions;
 const { getForUser: publicsForUser } = publicActions;
 
 class Publicaciones extends React.Component {
   async componentDidMount() {
+
+    const {
+      usersGetAll,
+      publicsForUser,
+      match:{ params: { key } }
+    } = this.props;
+
     if (!this.props.usersReducer.users.length) {
-      await this.props.usersGetAll();
+      await usersGetAll();
     }
-    this.props.publicsForUser(this.props.match.params.key);
+
+    if(this.props.usersReducer.error){
+      return;
+    }
+
+    if(!('publics_key' in this.props.usersReducer.users[key])){
+      publicsForUser(key);
+    }
+  }
+  
+  putUser = () =>{
+    const { usersReducer, match:{ params: { key } }} = this.props;
+
+    if(usersReducer.error){
+      return <Error message={usersReducer.error} />
+    }
+    
+    if(!usersReducer.users.length || usersReducer.loading){
+      return <Spinner />
+    }
+
+    const name = usersReducer.users[key].name
+
+    return(
+      <h1>Publicaciones de {name} </h1>
+    )
+
   }
 
   render() {
     console.log(this.props);
     return (
       <React.Fragment>
-        <h1>Publicaciones de </h1>
         {this.props.match.params.key}
+        {this.putUser()}
       </React.Fragment>
     );
   }
